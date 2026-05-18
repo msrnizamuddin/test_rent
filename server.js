15135a22d4c20b86f3f6af4d3d5763314ebb5612
tomal7010 <badhonbiswast@gmail.com>
2026-05-19 01:00:42 +0600

    upload

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+import { env } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import router from "./route.js";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet());
+
+// routes
+app.use(router);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Server running 🚀");
+});
+
+app.get("/api/v1", (req, res) => {
+  res.json({ message: "API v1 working ✅" });
+});
+
+// start server
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(env.PORT, () => {
+      console.log(`🚀 Server running on port ${env.PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Server start failed:", err.message);
+  }
+};
+
+startServer();
---
+++ v1/modules/auth/route/index.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+// import auth from "./auth.route.js";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "Auth route working Good ✅" });
+});
+
+// router.use("/", auth);
+
+export default router;
