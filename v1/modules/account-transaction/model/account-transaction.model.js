075b9e47eb0418c7584af5bd2b7434d8521fe576
msrnizamuddin <msrnizamuddin@gmail.com>
2026-05-19 19:15:57 +0600

    auth an account-transaction models

---
+++ v1/modules/account-transaction/model/account-transaction.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const accountTransactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Auth",
+      required: true,
+    },
+
+    transactionType: {
+      type: String,
+      enum: ["deposit", "withdraw", "transfer"],
+      required: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "completed", "failed"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "AccountTransaction",
+  accountTransactionSchema
+);
