3a09d999eece8d18230e546ac19841e6945871cc
msrrahul77 <msrrahul77@gmail.com>
2026-06-05 17:20:50 +0600

    BrandCommit By Rahul

--- v1/modules/auth/model/auth.model.js
+++ v1/modules/auth/model/auth.model.js
@@ -2,82 +2,83 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const authSchema = new mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    tenantId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "tenant",
-      required: true,
-    },
+	{
+		fullName: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		tenantId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'tenant',
+			required: true
+		},
 
-    email: {
-      type: String,
-      required: true,
-      lowercase: true,
-      trim: true,
-    },
+		email: {
+			type: String,
+			required: true,
+			lowercase: true,
+			trim: true
+		},
 
-    password: {
-      type: String,
-      required: true,
-      minlength: 8,
-      select: false,
-    },
+		password: {
+			type: String,
+			required: true,
+			minlength: 8,
+			select: false
+		},
 
-    role: {
-      type: String,
-      enum: ["Super Admin", "Tenant"],
-      default: "Tenant",
-    },
+		role: {
+			type: String,
+			enum: ['Super Admin', 'Tenant'],
+			default: 'Tenant'
+		},
 
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
-    centralStatus: {
-      type: String,
-      enum: ["active", "inactive"],
-      default: "active",
-    },
-    domain: {
-      type: String,
-      trim: true,
-    },
-    clientLoginToken: {
-      type: String,
-    },
-    tokenExpiration: {
-      type: Date,
-    },
-    supportedLanguages: {
-      type: [String],
-      default: [],
-    },
-    supportedCurrency: {
-      type: [String],
-      default: [],
-    },
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "auth",
-    },
-    updatedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "auth",
-    },
-    verificationToken: {
-      type: String,
-      select: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
+		isActive: {
+			type: Boolean,
+			default: true
+		},
+		centralStatus: {
+			type: String,
+			enum: ['active', 'inactive'],
+			default: 'active'
+		},
+		domain: {
+			type: String,
+			trim: true
+		},
+		clientLoginToken: {
+			type: String
+		},
+		tokenExpiration: {
+			type: Date
+		},
+		supportedLanguages: {
+			type: [String],
+			default: []
+		},
+		supportedCurrency: {
+			type: [String],
+			default: []
+		},
+		createdBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'auth'
+		},
+		updatedBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'auth'
+		},
+		verificationToken: {
+			type: String,
+			select: false
+		}
+	},
+	{
+		timestamps: true
+	}
 );
+
 
 authSchema.index({ tenantId: 1, email: 1 }, { unique: true });
 
--- v1/modules/brands/route/index.js
+++ v1/modules/brands/route/index.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import createBrand from "../controller/createBrand.js";
+import brandController from '../controller/createBrand.js';
+import validate from '../middleware/validate.js';
+import schema from '../validation/brand.validation.js';
 const router = Router();
 
-router.post("/createBrand",createBrand );
-
+router.post('/createBrand', validate(schema), brandController.createBrand);
+router.get('/getBrand', brandController.getBrands);
+router.get('/getBrand/:id', brandController.getBrandById);
+router.patch('/update/:id', brandController.updateBrand);
+router.delete('/delete/:id', brandController.deleteBrand);
+router.put('/update/:id', brandController.updateBrand);
 
 
 export default router;
